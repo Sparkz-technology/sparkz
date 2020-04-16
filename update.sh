612c9af0ae48a637096455c71233939a21564401
sutharsan sparkz <62829664+Sparkz-technology@users.noreply.github.com>
2020-04-16 15:33:47 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,53 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+
+echo " "
+echo " "
+sleep 6.0
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo '
+                                                            
+                           ▄▄                               
+                           ██              ██               
+ ██    ██  ██▄███▄    ▄███▄██   ▄█████▄  ███████    ▄████▄  
+ ██    ██  ██▀  ▀██  ██▀  ▀██   ▀ ▄▄▄██    ██      ██▄▄▄▄██ 
+ ██    ██  ██    ██  ██    ██  ▄██▀▀▀██    ██      ██▀▀▀▀▀▀ 
+ ██▄▄▄███  ███▄▄██▀  ▀██▄▄███  ██▄▄▄███    ██▄▄▄   ▀██▄▄▄▄█ 
+  ▀▀▀▀ ▀▀  ██ ▀▀▀      ▀▀▀ ▀▀   ▀▀▀▀ ▀▀     ▀▀▀▀     ▀▀▀▀▀  
+           ██                                               
+                                                            
+
+                  
+'|lolcat
+sleep 2.0
+cd $HOME
+rm -rf sparkz
+git clone https://github.com/sparkz-technology/sparkz
+clear
+echo " "
+echo -e "$grn               sparkz has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd sparkz
+clear
+bash sparkz
+sleep 1.0
+clear
